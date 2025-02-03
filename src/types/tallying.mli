25bc6874488c9b9472eb5732309b13d38783d014
Mickael Laurent <esh4rk@gmail.com>
2025-02-03 22:52:05 +0100

    doc

--- src/types/tallying.mli
+++ src/types/tallying.mli
@@ -11,12 +11,12 @@ val tally : VarSet.t -> constr list -> Subst.t list
 (** [tally_with_order compare mono constrs] is the same as [tally mono constrs],
 but using the total order [compare] over type variables. The solutions returned
 are such that a variable cannot be substituted by a type featuring a smaller
-non-monomorphic variable. *)
+non-monomorphic variable at top-level. *)
 val tally_with_order : (Var.t -> Var.t -> int) -> VarSet.t -> constr list -> Subst.t list
 
 (** [tally_with_priority lst mono constrs] is the same as [tally mono constrs],
 but using an order that will preserve variables in [lst] when possible.
 The solutions returned are such that a variable in [lst] cannot be substituted
-by a type featuring a non-monomorphic variable further in [lst] or not in [lst].
-The list of variables [lst] should not have duplicates. *)
+by a type featuring, at top-level, a non-monomorphic variable further in [lst]
+or not in [lst]. The list of variables [lst] should not have duplicates. *)
 val tally_with_priority : Var.t list -> VarSet.t -> constr list -> Subst.t list
