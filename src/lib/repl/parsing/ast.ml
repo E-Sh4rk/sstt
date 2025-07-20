3c8b8cc1434a6a6157f66dc190887ffcb20e35ee
Kim Nguyễn <kn@lmf.cnrs.fr>
2025-07-20 23:12:28 +0200

    Rename `Atoms` (the component) to `Enums`
    Avoid the ambiguity between `Atoms` (i.e. constants) and
    atoms of a DNF/BDD.

--- src/lib/repl/parsing/ast.ml
+++ src/lib/repl/parsing/ast.ml
@@ -2,7 +2,7 @@
 open Sstt
 
 type builtin =
-  | TEmpty | TAny | TAnyTuple | TAnyAtom | TAnyTag | TAnyInt
+  | TEmpty | TAny | TAnyTuple | TAnyEnum | TAnyTag | TAnyInt
   | TAnyArrow | TAnyRecord | TAnyTupleComp of int | TAnyTagComp of TagComp.Tag.t
 type varop = TTuple
 type binop = TCap | TCup | TDiff | TArrow
@@ -42,17 +42,17 @@ type command = Elt of elt | End
 
 module StrMap = Map.Make(String)
 
-type env = { aenv : Atoms.Atom.t StrMap.t ;
+type env = { eenv : Enums.Atom.t StrMap.t ;
              tagenv : TagComp.Tag.t StrMap.t ;
              tenv : Ty.t StrMap.t ;
              venv : Var.t StrMap.t ;
              mvenv : Var.t StrMap.t ;
              mono : VarSet.t ;
              lenv : Label.t StrMap.t
-}
+           }
 
 let empty_env = {
-  aenv = StrMap.empty ; tagenv = StrMap.empty ;
+  eenv = StrMap.empty ; tagenv = StrMap.empty ;
   tenv = StrMap.empty ;
   venv = StrMap.empty ; mvenv = StrMap.empty ;
   mono = VarSet.empty ; lenv = StrMap.empty
@@ -61,7 +61,7 @@ let empty_env = {
 let builtin t =
   match t with
   | TEmpty -> Ty.empty | TAny -> Ty.any
-  | TAnyAtom -> Descr.mk_atoms Atoms.any |> Ty.mk_descr
+  | TAnyEnum -> Descr.mk_enums Enums.any |> Ty.mk_descr
   | TAnyTag -> Descr.mk_tags Tags.any |> Ty.mk_descr
   | TAnyInt -> Descr.mk_intervals Intervals.any |> Ty.mk_descr
   | TAnyTuple -> Descr.mk_tuples Tuples.any |> Ty.mk_descr
@@ -72,46 +72,46 @@ let builtin t =
 
 let tvar env str =
   begin match StrMap.find_opt str env.venv with
-  | Some v -> v, env
-  | None ->
-    let v = Var.mk str in
-    let venv = StrMap.add str v env.venv in
-    let env = { env with venv } in
-    v, env
+    | Some v -> v, env
+    | None ->
+      let v = Var.mk str in
+      let venv = StrMap.add str v env.venv in
+      let env = { env with venv } in
+      v, env
   end
 
 let tvar_mono env str =
   begin match StrMap.find_opt str env.mvenv with
-  | Some v -> v, env
-  | None ->
-    let v = Var.mk str in
-    let mvenv = StrMap.add str v env.mvenv in
-    let mono = VarSet.add v env.mono in
-    let env = { env with mvenv ; mono } in
-    v, env
+    | Some v -> v, env
+    | None ->
+      let v = Var.mk str in
+      let mvenv = StrMap.add str v env.mvenv in
+      let mono = VarSet.add v env.mono in
+      let env = { env with mvenv ; mono } in
+      v, env
   end
 
 let label env str =
   begin match StrMap.find_opt str env.lenv with
-  | Some l -> l, env
-  | None ->
-    let l = Label.mk str in
-    let lenv = StrMap.add str l env.lenv in
-    let env = { env with lenv } in
-    l, env
+    | Some l -> l, env
+    | None ->
+      let l = Label.mk str in
+      let lenv = StrMap.add str l env.lenv in
+      let env = { env with lenv } in
+      l, env
   end
 
 let type_or_atom env str =
   match StrMap.find_opt str env.tenv with
   | Some t -> t, env
   | None ->
-    begin match StrMap.find_opt str env.aenv with
-    | Some a -> Descr.mk_atom a |> Ty.mk_descr, env
-    | None ->
-      let a = Atoms.Atom.mk str in
-      let aenv = StrMap.add str a env.aenv in
-      let env = { env with aenv } in
-      Descr.mk_atom a |> Ty.mk_descr, env  
+    begin match StrMap.find_opt str env.eenv with
+      | Some a -> Descr.mk_enum a |> Ty.mk_descr, env
+      | None ->
+        let a = Enums.Atom.mk str in
+        let eenv = StrMap.add str a env.eenv in
+        let env = { env with eenv } in
+        Descr.mk_enum a |> Ty.mk_descr, env  
     end
 
 let tag env str ty =
@@ -125,103 +125,103 @@ let tag env str ty =
 
 let build_ty env t =
   let rec aux env t =
-  match t with
-  | TBuiltin b -> builtin b, env
-  | TNamed str -> type_or_atom env str
-  | TTag (str, ty) ->
-    let ty, env = aux env ty in
-    tag env str ty
-  | TInterval (lb, ub) ->
-    Intervals.Atom.mk lb ub |> Descr.mk_interval |> Ty.mk_descr, env
-  | TVar str ->
-    let v, env = tvar env str in
-    Ty.mk_var v, env
-  | TVarMono str ->
-    let v, env = tvar_mono env str in
-    Ty.mk_var v, env
-  | TRecord (bindings, opened) ->
-    let (bindings,env) =
-      List.fold_left (fun (res, env) (l,ty,b) ->
-        let l, env = label env l in
-        let ty, env = aux env ty in
-        (l,(ty,b))::res,env
-      ) ([], env) bindings in
-    let bindings = LabelMap.of_list bindings in
-    Descr.mk_record { bindings ; opened } |> Ty.mk_descr, env
-  | TVarop (v, tys) ->
-    let (tys,env) =
-      List.fold_left (fun (res, env) ty ->
-        let ty, env = aux env ty in
-        ty::res,env
-      ) ([], env) tys in
-    let tys = List.rev tys in
-    begin match v with
-    | TTuple -> Descr.mk_tuple tys |> Ty.mk_descr, env
-    end
-  | TBinop (b, ty1, ty2) ->
-    let ty1, env = aux env ty1 in
-    let ty2, env = aux env ty2 in
-    begin match b with
-    | TArrow -> Descr.mk_arrow (ty1, ty2) |> Ty.mk_descr, env
-    | TCap -> Ty.cap ty1 ty2, env
-    | TCup -> Ty.cup ty1 ty2, env
-    | TDiff -> Ty.diff ty1 ty2, env
-    end
-  | TUnop (u,ty) ->
-    let ty, env = aux env ty in
-    begin match u with
-    | TNeg -> Ty.neg ty, env
-    end
-  | TWhere (ty, defs) ->
-    let vars = defs |> List.map
-      (fun (name, _) -> name, Var.mk name) |> StrMap.of_list in
-    let env = ref env in
-    List.iter (fun (name, _) ->
-      let tenv = StrMap.add name (Ty.mk_var (StrMap.find name vars)) !env.tenv in
-      env := {(!env) with tenv}
-    ) defs ;
-    let eqs = defs |> List.map (fun (name, ty) ->
+    match t with
+    | TBuiltin b -> builtin b, env
+    | TNamed str -> type_or_atom env str
+    | TTag (str, ty) ->
+      let ty, env = aux env ty in
+      tag env str ty
+    | TInterval (lb, ub) ->
+      Intervals.Atom.mk lb ub |> Descr.mk_interval |> Ty.mk_descr, env
+    | TVar str ->
+      let v, env = tvar env str in
+      Ty.mk_var v, env
+    | TVarMono str ->
+      let v, env = tvar_mono env str in
+      Ty.mk_var v, env
+    | TRecord (bindings, opened) ->
+      let (bindings,env) =
+        List.fold_left (fun (res, env) (l,ty,b) ->
+            let l, env = label env l in
+            let ty, env = aux env ty in
+            (l,(ty,b))::res,env
+          ) ([], env) bindings in
+      let bindings = LabelMap.of_list bindings in
+      Descr.mk_record { bindings ; opened } |> Ty.mk_descr, env
+    | TVarop (v, tys) ->
+      let (tys,env) =
+        List.fold_left (fun (res, env) ty ->
+            let ty, env = aux env ty in
+            ty::res,env
+          ) ([], env) tys in
+      let tys = List.rev tys in
+      begin match v with
+        | TTuple -> Descr.mk_tuple tys |> Ty.mk_descr, env
+      end
+    | TBinop (b, ty1, ty2) ->
+      let ty1, env = aux env ty1 in
+      let ty2, env = aux env ty2 in
+      begin match b with
+        | TArrow -> Descr.mk_arrow (ty1, ty2) |> Ty.mk_descr, env
+        | TCap -> Ty.cap ty1 ty2, env
+        | TCup -> Ty.cup ty1 ty2, env
+        | TDiff -> Ty.diff ty1 ty2, env
+      end
+    | TUnop (u,ty) ->
+      let ty, env = aux env ty in
+      begin match u with
+        | TNeg -> Ty.neg ty, env
+      end
+    | TWhere (ty, defs) ->
+      let vars = defs |> List.map
+                   (fun (name, _) -> name, Var.mk name) |> StrMap.of_list in
+      let env = ref env in
+      List.iter (fun (name, _) ->
+          let tenv = StrMap.add name (Ty.mk_var (StrMap.find name vars)) !env.tenv in
+          env := {(!env) with tenv}
+        ) defs ;
+      let eqs = defs |> List.map (fun (name, ty) ->
+          let (ty, env') = aux !env ty in
+          env := env' ;
+          (StrMap.find name vars, ty)
+        ) in
+      let tys = Ty.of_eqs eqs |> VarMap.of_list in
+      List.iter (fun (name, _) ->
+          let ty = VarMap.find (StrMap.find name vars) tys in
+          let tenv = StrMap.add name ty !env.tenv in
+          env := {(!env) with tenv}
+        ) defs ;
       let (ty, env') = aux !env ty in
       env := env' ;
-      (StrMap.find name vars, ty)
-    ) in
-    let tys = Ty.of_eqs eqs |> VarMap.of_list in
-    List.iter (fun (name, _) ->
-      let ty = VarMap.find (StrMap.find name vars) tys in
-      let tenv = StrMap.add name ty !env.tenv in
-      env := {(!env) with tenv}
-    ) defs ;
-    let (ty, env') = aux !env ty in
-    env := env' ;
-    List.iter (fun (name, _) ->
-      let tenv = StrMap.remove name !env.tenv in
-      env := {(!env) with tenv}
-    ) defs ;
-    ty, !env
+      List.iter (fun (name, _) ->
+          let tenv = StrMap.remove name !env.tenv in
+          env := {(!env) with tenv}
+        ) defs ;
+      ty, !env
   in
   aux env t
 
 let build_subst env s =
   let env = ref env in
   let s = s |> List.map (fun (b,v,ty) ->
-    let v, env' =
-      if b then tvar_mono !env v else tvar !env v in
-    let ty, env' = build_ty env' ty in
-    env := env' ; (v,ty)
-  ) |> Subst.of_list
+      let v, env' =
+        if b then tvar_mono !env v else tvar !env v in
+      let ty, env' = build_ty env' ty in
+      env := env' ; (v,ty)
+    ) |> Subst.of_list
   in
   s, !env
 
 let build_tally env cs =
   let env = ref env in
   let cs = cs |> List.map (fun (ty1,op,ty2) ->
-    let ty1, env' = build_ty !env ty1 in
-    let ty2, env' = build_ty env' ty2 in
-    env := env' ;
-    match op with
-    | LEQ -> [ty1,ty2]
-    | GEQ -> [ty2,ty1]
-    | EQ -> [ty1,ty2 ; ty2,ty1]
-  ) |> List.concat
+      let ty1, env' = build_ty !env ty1 in
+      let ty2, env' = build_ty env' ty2 in
+      env := env' ;
+      match op with
+      | LEQ -> [ty1,ty2]
+      | GEQ -> [ty2,ty1]
+      | EQ -> [ty1,ty2 ; ty2,ty1]
+    ) |> List.concat
   in
   cs, !env
