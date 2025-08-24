b0523b00db8f23ed24ab1e29f577d31e3966ccb9
Mickael Laurent <esh4rk@gmail.com>
2025-08-24 12:25:17 +0200

    add 'properties' field to Tags

--- src/lib/sstt/core/base.ml
+++ src/lib/sstt/core/base.ml
@@ -13,13 +13,38 @@ module LabelMap = Map.Make(Label)
 
 
 (** @canonical Sstt.Tag *)
-module Tag = Id.NamedIdentifier()
-(** Tags used for tagged type. *)
+module Tag : sig
+  include Id.NamedIdentifier
+  type prop =
+  | NoProperty
+  | Monotonic of { preserves_cup:bool ; preserves_cap:bool }
+
+  val mk' : string -> prop -> t
+  val properties : t -> prop
+end = struct
+  module I = Id.NamedIdentifier()
+  type prop =
+  | NoProperty
+  | Monotonic of { preserves_cup:bool ; preserves_cap:bool }
+
+  type t = I.t * prop
+  let default_prop = Monotonic { preserves_cap=true ; preserves_cup=true }
+  let mk name =  (I.mk name, default_prop)
+  let mk' name prop =  (I.mk name, prop)
+  let name (i,_) = I.name i
+  let properties (_,p) = p
+  let hash (i,_) = I.hash i
+  let compare (i1,_) (i2,_) = I.compare i1 i2
+  let equal (i1,_) (i2,_) = I.equal i1 i2
+  let pp fmt (i,_) = Format.fprintf fmt "%a" I.pp i
+  let pp_unique fmt (i,_) = Format.fprintf fmt "%a" I.pp i
+end
+(** Identifiers used for tagged type. *)
 
 
 (** @canonical Sstt.Enum *)
 module Enum = Id.NamedIdentifier()
-(** Identifiers used for enums. *)
+(** Identifiers used for enums type. *)
 
 
 (** @canonical Sstt.Var *)
