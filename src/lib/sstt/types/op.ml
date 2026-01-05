f1e3530f7ade4f82f799a53d3c1c14ee13cad80d
Mickael Laurent <esh4rk@gmail.com>
2026-01-05 17:01:30 +0100

    add module Op.Records'

--- src/lib/sstt/types/op.ml
+++ src/lib/sstt/types/op.ml
@@ -69,6 +69,76 @@ module TupleComp = struct
   let merge a1 a2 = a1@a2
 end
 
+module Records' = struct
+  module Atom = struct
+    module LabelMap = Map.Make(Label)
+    type t = { bindings : Ty.F.t LabelMap.t ; tail : Ty.F.t }
+    let dom t = LabelMap.bindings t.bindings |> List.map fst |> LabelSet.of_list
+    let find lbl t =
+      match LabelMap.find_opt lbl t.bindings with
+      | Some f -> f
+      | None -> t.tail
+  end
+
+  type t = Records.t
+  type atom = Atom.t
+
+  let as_union t =
+    Records.dnf' t |> List.map (fun t ->
+        let bindings = t.Records.Atom'.bindings |> LabelMap.bindings
+        |> List.map (fun (lbl,f) -> lbl, f) |> Atom.LabelMap.of_list in
+        { Atom.bindings; tail=t.tail }
+      )
+
+  let conv_atom { Atom.bindings ; tail } =
+    let bindings = bindings |> Atom.LabelMap.bindings
+        |> List.map (fun (lbl,f) -> lbl, f) |> LabelMap.of_list in
+    { Records.Atom.bindings ; tail=tail }
+
+  let of_union lst =
+    Records.of_dnf (List.map (fun a -> [conv_atom a],[]) lst)
+  let of_atom a = [[conv_atom a],[]] |> Records.of_dnf
+
+  let approx t =
+    let open Atom in
+    let union_a a1 a2 =
+      let dom = LabelSet.union (Atom.dom a1) (Atom.dom a2) in
+      let bindings = dom |> LabelSet.to_list |> List.map (fun lbl ->
+          (lbl, Ty.F.cup (Atom.find lbl a1) (Atom.find lbl a2))
+        ) |> LabelMap.of_list in
+      { Atom.bindings ; Atom.tail = Ty.F.cup a1.tail a2.tail }
+    in
+    match as_union t with
+    | [] -> raise EmptyAtom
+    | hd::tl -> List.fold_left union_a hd tl
+
+  let proj label t =
+    as_union t |> List.map (Atom.find label) |> Ty.F.disj
+
+  let merge a1 a2 =
+    let open Atom in
+    let dom = LabelSet.union (Atom.dom a1) (Atom.dom a2) in
+    let is_opt f = Ty.F.get_descr f |> Ty.O.is_optional in
+    let present = Ty.F.mk_descr (Ty.O.required Ty.any) in
+    let bindings = dom |> LabelSet.to_list |> List.map (fun lbl ->
+        let oty1, oty2 = Atom.find lbl a1, Atom.find lbl a2 in
+        let oty = if is_opt oty2 then
+          Ty.F.cup oty1 (Ty.F.cap oty2 present)
+        else oty2 in
+        (lbl, oty)
+      ) |> LabelMap.of_list in
+    let tail = if is_opt a2.tail then
+      Ty.F.cup a1.tail (Ty.F.cap a2.tail present)
+    else a2.tail in
+    { bindings ; tail } |> of_atom
+
+  let remove a lbl =
+    let open Atom in
+    let bindings = a.Atom.bindings |> LabelMap.add lbl (Ty.O.absent |> Ty.F.mk_descr) in
+    { bindings ; tail=a.tail } |> of_atom
+
+end
+
 module Records = struct
   module Atom = struct
     module LabelMap = Map.Make(Label)
@@ -83,54 +153,32 @@ module Records = struct
   type t = Records.t
   type atom = Atom.t
 
-  let as_union t =
-    Records.dnf' t |> List.map (fun t ->
-        let bindings = t.Records.Atom'.bindings |> LabelMap.bindings
-        |> List.map (fun (lbl,f) -> lbl,Ty.F.get_descr f) |> Atom.LabelMap.of_list in
-        { Atom.bindings; tail=Ty.F.get_descr t.tail }
-      )
-
-  let conv_atom { Atom.bindings ; tail } =
-    let bindings = bindings |> Atom.LabelMap.bindings
-        |> List.map (fun (lbl,f) -> lbl,Ty.F.mk_descr f) |> LabelMap.of_list in
-    { Records.Atom.bindings ; tail=Ty.F.mk_descr tail }
-
-  let of_union lst =
-    Records.of_dnf (List.map (fun a -> [conv_atom a],[]) lst)
-  let of_atom a = [[conv_atom a],[]] |> Records.of_dnf
-
-  let approx t =
+  let ignore_fields { Records'.Atom.bindings ; tail } =
+    let open Records'.Atom in
+    let bindings = LabelMap.map Ty.F.get_descr bindings in
+    let tail = Ty.F.get_descr tail in
+    { Atom.bindings ; tail }
+  let mk_fields { Atom.bindings ; tail } =
     let open Atom in
-    let union_a a1 a2 =
-      let dom = LabelSet.union (Atom.dom a1) (Atom.dom a2) in
-      let bindings = dom |> LabelSet.to_list |> List.map (fun lbl ->
-          (lbl, Ty.O.cup (Atom.find lbl a1) (Atom.find lbl a2))
-        ) |> LabelMap.of_list in
-      { Atom.bindings ; Atom.tail = Ty.O.cup a1.tail a2.tail }
-    in
-    match as_union t with
-    | [] -> raise EmptyAtom
-    | hd::tl -> List.fold_left union_a hd tl
+    let bindings = LabelMap.map Ty.F.mk_descr bindings in
+    let tail = Ty.F.mk_descr tail in
+    { Records'.Atom.bindings ; tail }
 
-  let proj label t =
-    as_union t |> List.map (Atom.find label) |> Ty.O.disj
+  let as_union t =
+    Records'.as_union t |> List.map ignore_fields
+  let of_union lst =
+    List.map mk_fields lst |> Records'.of_union
+  let of_atom a = mk_fields a |> Records'.of_atom
+
+  let approx t = Records'.approx t |> ignore_fields
+
+  let proj label t = Records'.proj label t |> Ty.F.get_descr
 
   let merge a1 a2 =
-    let open Atom in
-    let dom = LabelSet.union (Atom.dom a1) (Atom.dom a2) in
-    let bindings = dom |> LabelSet.to_list |> List.map (fun lbl ->
-        let oty1, oty2 = Atom.find lbl a1, Atom.find lbl a2 in
-        let oty = if snd oty2 then Ty.O.cup oty1 (fst oty2, false) else oty2 in
-        (lbl, oty)
-      ) |> LabelMap.of_list in
-    let tail = if snd a2.tail then Ty.O.cup a1.tail (fst a2.tail, false) else a2.tail in
-    { bindings ; tail } |> of_atom
+    Records'.merge (mk_fields a1) (mk_fields a2)
 
   let remove a lbl =
-    let open Atom in
-    let bindings = a.Atom.bindings |> LabelMap.add lbl (Ty.O.absent) in
-    { bindings ; tail=a.tail } |> of_atom
-
+    Records'.remove (mk_fields a) lbl
 end
 
 module TagComp = struct
