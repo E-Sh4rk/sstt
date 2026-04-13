6202a5eab45a27ad1769433514da4caa1e13665f
FredBelanger09 <fred.belanger09@laposte.net>
2026-04-13 17:40:42 +0200

    Ajout de la mémoïsation et nettoyage du code

--- src/lib/sstt/types/witness.ml
+++ src/lib/sstt/types/witness.ml
@@ -1,232 +1,191 @@
 open Core
 
-type single_t = Int of Z.t
-|Enum of Enums.Atom.t
-|Arrow of Arrows.t
-|Tuple of single_t list
-|Tag of (Tag.t * single_t)
-|Other
-|Wrong
-|Empty
+type t = Int of Z.t
+       | Enum of Enums.Atom.t
+       | Arrow of Arrows.t
+       | Tuple of t list
+       | Tag of (Tag.t * t)
+       | Other
+       | Wrong
 
+module VDHash = Hashtbl.Make(Descr)
+let hash = VDHash.create 16
 
-let rec singl_to_Ty s =
+let rec to_ty s =
   match s with 
-  Int i -> Intervals.Atom.mk_singl i |> Descr.mk_interval |> VDescr.mk_descr |> Ty.of_def
-  |Enum e -> Descr.mk_enum e|> VDescr.mk_descr |> Ty.of_def
-  |Tuple tu -> List.map singl_to_Ty tu |> Descr.mk_tuple |> VDescr.mk_descr |> Ty.of_def
-  |Tag (ta,ty) -> (ta, singl_to_Ty ty) |> Descr.mk_tag |> VDescr.mk_descr |> Ty.of_def
-  |Arrow a -> Descr.mk_arrows a |> VDescr.mk_descr |> Ty.of_def
-  |Other -> Descr.mk_others true |> VDescr.mk_descr |> Ty.of_def
-  |Wrong -> Ty.empty
-  |Empty -> Ty.empty
-  
+    Int i -> Intervals.Atom.mk_singl i |> Descr.mk_interval |> Ty.mk_descr
+  | Enum e -> Descr.mk_enum e|> Ty.mk_descr
+  | Tuple tu -> List.map to_ty tu |> Descr.mk_tuple |> Ty.mk_descr
+  | Tag (ta,ty) -> (ta, to_ty ty) |> Descr.mk_tag |> Ty.mk_descr
+  | Arrow a -> Descr.mk_arrows a |> Ty.mk_descr
+  | Other -> Descr.mk_others true |> Ty.mk_descr
+  | Wrong -> Ty.any
+
+
+let pp fmt s = match s with
+    Int i -> Z.pp_print fmt i
+  | Enum s -> Format.fprintf fmt "\" %a \"" Printer.print_ty' (to_ty (Enum s))
+  | Tuple t -> Format.fprintf fmt "( %a )" Printer.print_ty' (to_ty (Tuple t ))
+  | Tag ta -> Printer.print_ty' fmt (to_ty (Tag ta))
+  | Arrow a -> Format.fprintf fmt "fun :< %a -> %a >" Printer.print_ty'  (Op.Arrows.dom a) Printer.print_ty'  (Op.Arrows.apply a (Op.Arrows.dom a))
+  | Other -> Format.pp_print_string fmt "Other"
+  | Wrong -> Format.pp_print_string fmt "NOT IMPLEMENTED YET"
+
+
+
+let rec equal t1 t2 =
+  match (t1,t2) with 
+    Int t1, Int t2 -> Z.equal t1 t2
+  | Enum e1, Enum e2 -> Enum.equal e1 e2
+  | Tuple t1, Tuple t2 -> begin
+      try List.for_all2 equal t1 t2 with _ -> false
+    end
+  | Tag (ta1, ty1), Tag (ta2,ty2) -> Tag.equal ta1 ta2 && equal ty1 ty2
+  | Arrow t1, Arrow t2 -> Arrows.equal t1 t2
+  | _ -> false
+
+let is_in singl t =
+  Ty.leq (to_ty singl) t
+
+
 let mk_intervals t = 
   let t_interval = Ty.get_descr t |> Descr.get_intervals |> Intervals.destruct |> List.hd |> Intervals.Atom.get in 
   match t_interval with 
-    |(None, None) ->  Int (Z.of_int 42)
-    |(Some z1, _) | (None, Some z1) -> Int z1
-  
+  | (None, None) ->  Int (Z.of_int 42)
+  | (Some z1, _) | (None, Some z1) -> Int z1
+
 let mk_enums t = 
   let t_enum = Ty.get_descr t |> Descr.get_enums |> Enums.destruct in 
   match t_enum with
-    |(true, lt_enum) -> Enum (List.hd lt_enum)
-    |(false, lt_enum) -> Enum (Enums.Atom.mk (String.make ((List.fold_left (fun a b -> max a (String.length (Enums.Atom.name b))) 0 lt_enum )+1) 'a'))
-  
+  | (true, lt_enum) -> Enum (List.hd lt_enum)
+  | (false, lt_enum) -> Enum (Enums.Atom.mk (String.make ((List.fold_left (fun a b -> max a (String.length (Enums.Atom.name b))) 0 lt_enum )+1) 'a'))
 
-    
+
+
 let mk_arrows t = 
-    let a1,a2 = Ty.get_descr t |> Descr.get_arrows |> Arrows.dnf |> List.hd in
-    let rec help_arrow t a1 a2 a3 = 
-      let test_arrow = Arrows.of_dnf [(a1,a2)] |> Descr.mk_arrows |> Ty.mk_descr in if Ty.leq test_arrow t then Arrows.of_dnf [a1,a2] 
+  let a1,a2 = Ty.get_descr t |> Descr.get_arrows |> Arrows.dnf |> List.hd in
+  let rec help_arrow t a1 a2 a3 = 
+    let test_arrow = Arrows.of_dnf [(a1,a2)] |> Descr.mk_arrows |> Ty.mk_descr in if Ty.leq test_arrow t then Arrows.of_dnf [a1,a2] 
     else begin 
       match a3 with 
-      a:: l -> help_arrow t a1 (a :: a2) l
+        a:: l -> help_arrow t a1 (a :: a2) l
       |_ -> [Ty.get_descr t |> Descr.get_arrows |> Arrows.dnf |> List.hd] |> Arrows.of_dnf
-      end in
-     Arrow(help_arrow t a1 [] a2)
+    end in
+  Arrow(help_arrow t a1 [] a2)
 
-let rec mk_tuple_atoms make mem t len a1 a2 a3 = 
-  let test_tuple = Tuples.construct (true,[TupleComp.of_dnf len [(a1,a2)]]) |> Descr.mk_tuples |> Ty.mk_descr in 
-  if Ty.leq test_tuple t then let w = Tuple(List.map (fun a -> let _,w = make mem a in w) (List.hd (Op.TupleComp.as_union(TupleComp.of_dnf len [a1,a2] )))) in ((Ty.hash t)::mem,w)
-else begin 
-  match a3 with 
-  a:: l -> mk_tuple_atoms make mem t len a1 (a :: a2) l
-  |_ ->  let w = Tuple(List.map (fun a -> let _,w =make mem a in w) (List.hd (Op.TupleComp.as_union(TupleComp.of_dnf len [a1,a2] )))) in ((Ty.hash t)::mem,w)
-  end 
 
-let rec mk_tuple_atom_list_atom_list_list make mem t len pn_list = match pn_list with 
-(p,n) :: l -> let (mem',w) = mk_tuple_atoms make mem t len p [] n in if (w == Empty ) then mk_tuple_atom_list_atom_list_list make mem t len l else ((Ty.hash t)::mem',w)
-|[] -> (mem,Empty)
-let rec mk_tuplecomp_list make mem t lt_tuple = match lt_tuple with 
-a::l -> let pn_list = TupleComp.dnf a in 
-      let (mem',w) = mk_tuple_atom_list_atom_list_list make mem t (TupleComp.len a) pn_list in if (w == Empty) then mk_tuplecomp_list make mem t l else ((Ty.hash t)::mem',w) 
-|[] -> (mem,Empty)
+let mk_tag_atom make tag a = 
+  let w = make  (let _,b = List.hd (Op.TagComp.as_union(TagComp.of_dnf tag [a] )) in b)in 
+  match w with 
+  | None -> None 
+  | Some w -> Some (Tag(tag,w))
 
-let mk_tuples_mem mk_mem mem t = let t_tuple = Ty.get_descr t |> Descr.get_tuples |> Tuples.destruct in 
-match t_tuple with 
-  |(true, lt_tuple) -> mk_tuplecomp_list mk_mem mem t lt_tuple
-  |(false, lt_tuple) -> (Ty.hash t::mem,Tuple(List.init ((List.fold_left (fun a b -> max a (Tuples.Comp.len b)) 0 lt_tuple)+1) (fun x -> Int(Z.of_int x))))
 
-let rec mk_tag_atom make mem t tag a1 a2 a3 = 
-  let test_tag = Tags.construct (true,[TagComp.of_dnf tag [(a1,a2)]]) |> Descr.mk_tags |> Ty.mk_descr in 
-  if Ty.leq test_tag t then let _,w = make mem (let _,b =List.hd (Op.TagComp.as_union(TagComp.of_dnf tag [a1,a2] )) in b) in ((Ty.hash t)::mem,Tag(tag,w)) else
-  match a3 with 
-  a:: l -> mk_tag_atom make mem t tag a1 (a :: a2) l
-  |_ ->  let _,w = make mem (let _,b = List.hd (Op.TagComp.as_union(TagComp.of_dnf tag [a1,a2] )) in b) in ((Ty.hash t)::mem,Tag(tag,w))
-   
-
-let rec mk_tag_atoms_list_atoms_list_list make mem t tag pn_list =
+let rec mk_tag_atoms_list_atoms_list_list make tag pn_list =
   match pn_list with 
-  (p,n) :: l -> 
-    let (mem',w) = mk_tag_atom make mem t tag p [] n in 
-    if (w == Empty ) 
-      then mk_tag_atoms_list_atoms_list_list make mem t tag l 
-    else ((Ty.hash t)::mem',w)
-  |[] -> (mem,Empty)
-let rec mk_tagcomp_list make mem t lt_tag =
+    a :: l -> begin
+      let w = mk_tag_atom make tag a in 
+      match w with 
+      |None -> mk_tag_atoms_list_atoms_list_list make tag l 
+      |Some _ -> w
+
+    end
+  | [] -> None
+
+
+let rec mk_tagcomp_list make t lt_tag =
   match lt_tag with 
-  a::l -> let pn_list = TagComp.dnf a in 
-  let (mem',w) = mk_tag_atoms_list_atoms_list_list make mem t (TagComp.tag a) pn_list in if (w == Empty) then mk_tagcomp_list make mem t l else ((Ty.hash t)::mem',w) 
-|[] -> (mem,Empty)
+    a::l -> 
+    begin
+      let pn_list = TagComp.dnf a in 
+      let w = mk_tag_atoms_list_atoms_list_list make (TagComp.tag a) pn_list in
+      match w with 
+      | None -> mk_tagcomp_list make t l
+      | Some _ -> w
+    end
+  |[] -> None
 
-
-let mk_tag_mem mk_mem mem t = 
+let rec len_false_tag len l = 
+  if List.for_all (fun a -> (a |> TagComp.tag |> Tag.name |> String.length) != len) l 
+    then len 
+    else len_false_tag (len+1) l
+let mk_tag make t = 
   let t_tag = Ty.get_descr t |> Descr.get_tags |> Tags.destruct in 
   match t_tag with 
-  | (true,co_l) -> mk_tagcomp_list mk_mem mem t co_l
+  | (true,co_l) -> mk_tagcomp_list make t co_l
   | (false, co_l) -> 
-  let new_tag = Tag.mk (String.make ((List.fold_left (fun a b -> let b = TagComp.tag b |> Tag.name |> String.length in if (b > a) then b else a) 1 co_l)+1) 'a') in 
-  let new_type = Int(Z.of_int 16)  in let w = Tag(new_tag,new_type) in ((Ty.hash t)::mem, w)
+    let new_tag = 
+      Tag.mk (String.make (len_false_tag 1 co_l) 'a') in 
+    let new_type = Int(Z.of_int 16)  in  Some (Tag(new_tag,new_type))
 
 
-let rec mk_mem mem t = 
-  if List.mem (Ty.hash t) mem then (mem,Empty) 
-  else
+let mk_tuple_atoms make atom = 
+  let w = List.map (fun a -> make a ) atom in 
+  if List.mem (let x = None in x) w then None
+  else Some(Tuple(List.map (fun a -> match a with  |None -> failwith "impossible" |Some w -> w) w))
+
+let rec mk_tuplecomp_atom_list make atom_list =
+
+  match atom_list with 
+    a::l -> begin let w = mk_tuple_atoms make a in match w with 
+    | Some _ -> w
+    | None -> mk_tuplecomp_atom_list make l
+    end
+  | []-> None
+
+
+let rec mk_tuplecomp_list make lt_tuple = 
+  match lt_tuple with 
+    a :: l -> begin 
+      let atom_list = Op.TupleComp.as_union a in 
+      let w = mk_tuplecomp_atom_list make atom_list in
+      match w with 
+      | Some _ -> w
+      | None -> mk_tuplecomp_list make l 
+    end
+  |[]-> None
+
+let rec len_false_tuple len l = 
+  if List.for_all (fun a -> TupleComp.len a!= len) l 
+  then len 
+  else len_false_tuple (len+1) l
+let mk_tuples_mem mk_mem t = 
+  let t_tuple = Ty.get_descr t |> Descr.get_tuples |> Tuples.destruct in 
+  match t_tuple with 
+  | (true, lt_tuple) -> mk_tuplecomp_list mk_mem  lt_tuple
+  | (false, lt_tuple) -> Some (Tuple(List.init (len_false_tuple 1 lt_tuple) (fun x -> Int(Z.of_int x))))
+
+let rec mk_mem t = 
   let t_descr = Ty.get_descr t in 
-  if Descr.get_intervals t_descr |> Intervals.equal Intervals.empty |> not
-    then (mem,mk_intervals t)
-  else 
-  if Descr.get_enums t_descr |> Enums.equal Enums.empty |> not 
-    then (mem,mk_enums t)
-  else 
-  if Descr.get_arrows t_descr |> Descr.mk_arrows |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not
-    then (mem,mk_arrows t)
-  else 
-  if Descr.get_tags t_descr|> Descr.mk_tags |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not 
-    then mk_tag_mem mk_mem mem t
-  else
-  if Descr.get_tuples t_descr|> Descr.mk_tuples |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not
-    then mk_tuples_mem mk_mem mem t
-  else failwith "Type not implemented yet"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-let _testing make mem t = 
-  let t_tuple = Ty.get_descr t |> Descr.get_tuples |> Tuples.destruct in
-match t_tuple with 
-|(true, lt_tuple) -> 
-let rec inside_story make mem lt_tuple =
-  begin
-    match lt_tuple with 
-  |a::l -> if List.mem a mem then inside_story make mem l else Tuple(List.map (fun b -> make (a::mem) b) (List.hd (Op.TupleComp.as_union a)) )
-  |_ -> failwith "oskour"
-
-  end in inside_story make mem lt_tuple
-
-
-
-|(false, lt_tuple) -> Tuple(List.init ((List.fold_left (fun a b -> max a (Tuples.Comp.len b)) 0 lt_tuple)+1) (fun x -> Int(Z.of_int x)))
-
-let mk_tuples make mem t = let t_tuple = Ty.get_descr t |> Descr.get_tuples |> Tuples.destruct in 
-match t_tuple with 
-  |(true, lt_tuple) -> let fst_tuple = List.hd lt_tuple in  Tuple(List.map (fun a -> make mem a) (List.hd (Op.TupleComp.as_union fst_tuple)) )
-  |(false, lt_tuple) -> Tuple(List.init ((List.fold_left (fun a b -> max a (Tuples.Comp.len b)) 0 lt_tuple)+1) (fun x -> Int(Z.of_int x)))
-
-let mk_tags make mem t =
-  let t_tag = Ty.get_descr t |> Descr.get_tags |> Tags.destruct in 
-  match t_tag with 
-  | (true,co_l) -> let fst_tag = List.hd co_l in let ta,t = Op.TagComp.as_union fst_tag |> List.hd in Tag(ta, make mem t)
-  | (false, co_l) -> let new_tag = Tag.mk (String.make ((List.fold_left (fun a b -> let b = TagComp.tag b |> Tag.name |> String.length in if (b > a) then b else a) 1 co_l)+1) 'a') in 
-let new_type = Int(Z.of_int 16)  in Tag(new_tag,new_type)
-
-
-
-let rec _make mem t = 
-  let t_descr = Ty.get_descr t in 
-  if Descr.get_intervals t_descr |> Intervals.equal Intervals.empty |> not
-    then mk_intervals t
-  else 
-  if Descr.get_enums t_descr |> Enums.equal Enums.empty |> not 
-    then mk_enums t
-  else 
-  if Descr.get_tuples t_descr|> Descr.mk_tuples |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not
-    then mk_tuples _make mem t
-  else
-  if Descr.get_tags t_descr|> Descr.mk_tags |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not 
-    then mk_tags _make mem t
-  else
-  if Descr.get_arrows t_descr |> Descr.mk_arrows |> VDescr.mk_descr |> Ty.of_def |> Ty.is_empty |> not
-    then mk_arrows t
-  else 
-  if Descr.get_records t_descr |> Descr.mk_records |> VDescr.mk_descr |>Ty.of_def |> Ty.is_empty |> not
-    then Wrong
-  else
-  if Descr.get_others t_descr then Other
-  else failwith"empty type"
+  let w = VDHash.find_opt hash t_descr in
+  match w with 
+  | Some(None) -> None
+  | Some a -> a
+  | None ->
+    VDHash.add hash t_descr None;
+    if Descr.get_intervals t_descr |> Intervals.equal Intervals.empty |> not
+    then let w = Some (mk_intervals t) in VDHash.replace hash t_descr w; w
+    else 
+    if Descr.get_enums t_descr |> Enums.equal Enums.empty |> not 
+    then let w = Some (mk_enums t) in VDHash.replace hash t_descr w; w
+    else 
+    if Descr.get_arrows t_descr |> Descr.mk_arrows |> Ty.mk_descr |> Ty.is_empty |> not
+    then let w = Some (mk_arrows t) in VDHash.replace hash t_descr w; w
+    else 
+    if Descr.get_tags t_descr|> Descr.mk_tags |> Ty.mk_descr |> Ty.is_empty |> not 
+    then let w = mk_tag mk_mem t in VDHash.replace hash t_descr w; w
+    else
+    if Descr.get_tuples t_descr|> Descr.mk_tuples |> Ty.mk_descr |> Ty.is_empty |> not
+    then let w= mk_tuples_mem mk_mem t in VDHash.replace hash t_descr w; w
+    else
+    if Descr.get_records t_descr |> Descr.mk_records |> VDescr.mk_descr |>Ty.of_def |> Ty.is_empty |> not
+    then Some(Wrong)
+    else
+    if Descr.get_others t_descr then Some(Other)
+    else None
 
 let mk t = 
-  let _,w = mk_mem [] t in w
- 
- let pp fmt s = match s with
-  Int i -> Z.pp_print fmt i
-  |Enum s -> Format.pp_print_string fmt "\"";Printer.print_ty' fmt (singl_to_Ty (Enum s)); Format.pp_print_string fmt "\""
-  |Tuple t -> Format.pp_print_string fmt "(";Printer.print_ty' fmt (singl_to_Ty (Tuple t )); Format.pp_print_string fmt ")"
-  |Tag ta -> Printer.print_ty' fmt (singl_to_Ty (Tag ta))
-  |Arrow a -> Format.pp_print_string fmt "fun :< "; Printer.print_ty' fmt (Op.Arrows.dom a); Format.pp_print_string fmt " -> "; Printer.print_ty' fmt (Op.Arrows.apply a (Op.Arrows.dom a)); Format.pp_print_string fmt " >"
-  |Other -> Format.pp_print_string fmt "Other"
-  |Wrong -> Format.pp_print_string fmt "NOT IMPLEMENTED YET"
-  |Empty -> Format.pp_print_string fmt "Empty type"
-
-  
-  let rec equal t1 t2 =
-    match (t1,t2) with 
-    Int t1, Int t2 -> Z.equal t1 t2
-    |Enum e1, Enum e2 -> Enums.equal (Enums.mk e1) (Enums.mk e2)
-    |Tuple t1, Tuple t2 -> begin
-      match t1, t2 with 
-      a1::l1, a2::l2 -> equal a1 a2 && equal (Tuple l1)(Tuple l2) 
-      |_, _ -> true 
-    end
-    |Tag (ta1, ty1), Tag (ta2,ty2) -> Tag.equal ta1 ta2 && equal ty1 ty2
-    |Arrow t1, Arrow t2 -> Arrows.equal t1 t2
-    |_ -> false
-
-  let is_in singl t =
-    Ty.leq (singl_to_Ty singl) t
-
+  VDHash.reset hash;
+  match mk_mem t with 
+  | Some w -> w
+  | None -> failwith "Empty Type"
--- src/tests/test_witness.ml
+++ src/tests/test_witness.ml
@@ -1,67 +1,23 @@
 open Sstt
 
-type test_t = 
-|Int_test of Intervals.t
-|Enum_test of Enums.t
-|Tuple_test of Tuples.t
-|Tag_test of Tags.t
-|Arrow_test of Arrows.t
+let () = Format.set_margin Format.pp_infinity
+
+let true_t = Enum.mk("True") |> Descr.mk_enum |> Ty.mk_descr
+let false_t = Enum.mk("False") |> Descr.mk_enum |> Ty.mk_descr
 let int = Intervals.any |> Descr.mk_intervals |> VDescr.mk_descr |> Ty.of_def
-let bool = Enums.construct(true, [Enums.Atom.mk "True"; Enums.Atom.mk "False" ]) |> Descr.mk_enums |> VDescr.mk_descr |> Ty.of_def
-let print_good typ i w t = 
-  Format.printf "%s line %i : witness %a is an inhabitant of %a \n" 
-  typ
-  i
-  Witness.pp w
-  Printer.print_ty' t
+let bool = Ty.cup true_t false_t
 
-let print_bad typ i w t = 
-  Format.printf "Problem for %s line %i : witness %a encapsulated as %a is not an inhabitant of %a \n" 
-  typ
-  i
-  Witness.pp w
-  Printer.print_ty' (Witness.singl_to_Ty w)
-  Printer.print_ty' t
-
-let test to_test =
-  let t = 
-    match to_test with
-    Int_test i -> Descr.mk_intervals i
-    |Enum_test e -> Descr.mk_enums e 
-    |Tuple_test u -> Descr.mk_tuples u
-    |Tag_test ta -> Descr.mk_tags ta
-    |Arrow_test a-> Descr.mk_arrows a
-  in let t =  VDescr.mk_descr t|> Ty.of_def in
-  let w = Witness.mk t in
-  (t,w)
-
-let rec test_list_inter l i print= 
-  match l with 
-    to_test :: l -> let t,w = test (Int_test to_test) in if (Witness.is_in w t) then begin 
-      if print then print_good "Inter" i w t;test_list_inter l (i+1) print end else 
-      print_bad "Inter" i w t
-    
-    |[] -> ()   
 
 let ints = [
-      Intervals.any;
-      Intervals.Atom.mk (Some Z.one) None |> Intervals.mk;
-      Intervals.Atom.mk None (Some (Z.of_int 2)) |> Intervals.mk;
-      Intervals.Atom.mk_bounded (Z.of_int 7) (Z.of_int 10)|> Intervals.mk;
-      [Intervals.Atom.mk_bounded Z.one (Z.of_int 6); Intervals.Atom.mk_bounded (Z.of_int 9)(Z.of_int 12)] |> Intervals.construct
-    ]
+    Intervals.any;
+    Intervals.Atom.mk (Some Z.one) None |> Intervals.mk;
+    Intervals.Atom.mk None (Some (Z.of_int 2)) |> Intervals.mk;
+    Intervals.Atom.mk_bounded (Z.of_int 7) (Z.of_int 10)|> Intervals.mk;
+    [Intervals.Atom.mk_bounded Z.one (Z.of_int 6); Intervals.Atom.mk_bounded (Z.of_int 9)(Z.of_int 12)] |> Intervals.construct
+  ]
 let type_ints = List.map (fun t -> Descr.mk_intervals t |> VDescr.mk_descr |> Ty.of_def) ints
 
-let rec test_list_enums l i print = 
-  match l with 
-  to_test :: l -> let t,w = test (Enum_test to_test) in if (Witness.is_in w t) then 
-    begin if print then
-      print_good "Enum" i w t;
-      test_list_enums l (i+1) print
-    end else 
-    print_bad "Enum" i w t
-  |[] -> () 
-  
+
 let enums = [
   Enums.any;
   Enums.construct(true, [Enums.Atom.mk "True"; Enums.Atom.mk "False" ]); 
@@ -70,15 +26,6 @@ let enums = [
 ]
 let type_enums = List.map (fun t -> Descr.mk_enums t |> VDescr.mk_descr |> Ty.of_def) enums
 
-let rec test_list_tuples l i print = 
-  match l with 
-    to_test :: l -> let t,w = test (Tuple_test to_test) in if (Witness.is_in w t) then
-    begin
-      if print then print_good "Tuple" i w t;
-      test_list_tuples l (i+1) print
-    end
-  else print_bad "Tuple" i w t
-    |[] -> ()
 
 let tuples = [
   Tuples.any;
@@ -91,15 +38,7 @@ let tuples = [
   Tuples.construct (false, [Tuples.Comp.mk [int; bool; bool]])
 ]
 let type_tuple = List.map (fun t -> Descr.mk_tuples t |> VDescr.mk_descr |> Ty.of_def) tuples
-let rec _test_list_tags l i print =
-  match l with 
-    to_test :: l -> let t,w = test (Tag_test to_test) in if (Witness.is_in w t) then
-    begin
-      if print then print_good "Tag" i w t;
-      _test_list_tags l (i+1) print
-    end
-  else print_bad "Tag" i w t
-    |[] -> ()
+
 
 let tags = [
   Tags.any;
@@ -108,80 +47,145 @@ let tags = [
   Tags.construct(false, [TagComp.mk(Tag.mk "foo", int)]);
   Tags.construct(false, [TagComp.mk(Tag.mk "foo", int); TagComp.mk(Tag.mk "foo2", bool)]);
   Tags.construct(true, [TagComp.mk(Tag.mk "foo", int); TagComp.mk(Tag.mk "foo2", bool)]);
-  Tags.construct(true, [TagComp.mk(Tag.mk "foo", int); TagComp.mk(Tag.mk "foo2", Ty.empty)])
-  
+  Tags.construct(true, [TagComp.mk(Tag.mk "foo", int); TagComp.mk(Tag.mk "foo2", Ty.empty)]);
 ]
 let type_tags = List.map (fun t -> Descr.mk_tags t |> VDescr.mk_descr |> Ty.of_def) tags
-let rec test_list_arrows l i print=
-  match l with 
-  to_test :: l -> let t,w = test (Arrow_test to_test) in if (Witness.is_in w t) then
-  begin
-    if print then print_good "Arrow" i w t;
-    test_list_arrows l (i+1) print
-  end
-  else print_bad "Arrow" i w t
-  |[] -> ()
 
 
-let to_use =
+let tup_to_arrow =
   let rec creation l = match l with
 a :: b :: l -> (a,b) :: creation l 
 |_ -> [] 
 in creation type_tuple
-
 let arrows = [
   Arrows.any;
   Arrows.mk (int, int);
   Arrows.mk (List.hd type_tuple, List.hd (List.tl type_tuple ));
-  Arrows.of_dnf [(to_use, [])];
-  Arrows.of_dnf [[], to_use]
-
+  Arrows.of_dnf [(tup_to_arrow, [])];
+  Arrows.of_dnf [[], tup_to_arrow];
 ]
-let _type_arrows = List.map (fun t -> Descr.mk_arrows t |> VDescr.mk_descr |> Ty.of_def) arrows
-let all_types = type_ints @ type_enums @ type_tags @ type_tuple
+let type_arrows = List.map (fun t -> Descr.mk_arrows t |> VDescr.mk_descr |> Ty.of_def) arrows
 
-let rec test_list l i print =
-  match l with 
-    t :: l -> let w =  Witness.mk t in if (Witness.is_in w t) then
-    begin
-      if print then print_good "All_types" i w t;
-      test_list l (i+1) print
-    end
-  else print_bad "All_types" i w t
-    |[] -> ()
 
 let vx1 = Var.mk "'x1"
 let tx1 = Ty.mk_var vx1
 
-let bool_bool = Descr.mk_tuple [bool;bool] |> Ty.mk_descr
-let int_x1_tx1 = Descr.mk_tuple [int;tx1] |> Ty.mk_descr
-let int_x1_tx1_bool_bool = Ty.cup  int_x1_tx1 bool_bool
-
-let solved = Ty.of_eqs [(vx1, int_x1_tx1_bool_bool)]
-let int_bool_list = solved |> VarMap.of_list |> VarMap.find vx1
+let int_bool_list = Ty.of_eqs [(vx1,Ty.cup (Descr.mk_tuple [int;tx1] |> Ty.mk_descr) (Descr.mk_tuple [bool;bool] |> Ty.mk_descr))] |> VarMap.of_list |> VarMap.find vx1
 
 let vx2 = Var.mk "'x2"
 let tx2 = Ty.mk_var vx2
 let tag_bool = Descr.mk_tag (Tag.mk"foo",bool) |> Ty.mk_descr
 let tag_x2_tx2 = Descr.mk_tag (Tag.mk"oof",tx2) |> Ty.mk_descr
-let tag_x2_tx2_tag_bool = Ty.cup tag_bool tag_x2_tx2
-let rec_types = [ int_bool_list]
+let tag_x2_tx2_tag_bool = Ty.cup  tag_bool tag_x2_tx2
+let solved2 = Ty.of_eqs[(vx2, tag_x2_tx2_tag_bool)]
+let bool_tag_list = solved2 |> VarMap.of_list |> VarMap.find vx2
 
-(*
-let () = let rec prin l =
-  match l with 
-  (a,b) :: l -> Format.printf "%a -> %a\n" Printer.print_ty' a Printer.print_ty' b; prin l 
-  |_ -> () in prin to_use
-*)
+let nil = Enum.mk "Nil" |> Descr.mk_enum |> Ty.mk_descr
+let vt1 = Var.mk "'t1"
+let tt1 = Ty.mk_var vt1
+let int_t1_tt1 = Descr.mk_tuple [int;tt1] |> Ty.mk_descr
+let nil_int_t1_tt1 = Ty.cup nil int_t1_tt1
 
-let () = Format.printf "The type %a is empty : %b\n" Printer.print_ty' int_bool_list (int_bool_list |> Ty.get_descr |> Descr.get_arrows |> Descr.mk_arrows |> Ty.mk_descr |> Ty.is_empty)
-let () = test_list_inter ints 1 false; 
-test_list_enums enums 1 false;
-test_list_tuples tuples 1 false;
-(*test_list_tags tags 1 false;*)
-test_list_arrows arrows 1 false;
-test_list rec_types 1 true;
-test_list all_types 1 true
+let solvedt1 = Ty.of_eqs [(vt1, nil_int_t1_tt1)]
+let t1 = solvedt1 |> VarMap.of_list |>VarMap.find vt1
+let int_t1 = Descr.mk_tuple [int;t1] |> Ty.mk_descr
+let t2 = Descr.mk_tuple [int_t1;t1] |> Ty.mk_descr
+
+let rec_types = [ bool_tag_list; int_bool_list;t1; t2]
 
 
-(*Format.printf "Witness for Enum : %a is \"%a\"\n" Printer.print_ty' t Witness.pp w*)
+let list1 = [Ty.any;
+bool;
+true_t;
+false_t;
+]
+
+let vx1 = Var.mk "'x1"
+let tx1 = Ty.mk_var vx1
+let part1 = (Descr.mk_tag (Tag.mk "tag", tx1) |> Ty.mk_descr)
+let a =Ty.cup 
+(42|> Z.of_int|> Intervals.Atom.mk_singl |>Descr.mk_interval |> Ty.mk_descr)
+(part1)
+let a = Ty.of_eqs [(vx1, a)] |> VarMap.of_list |> VarMap.find vx1
+let list2 = [
+  Tags.mk (Tag.mk "tag", bool)|> Descr.mk_tags |> Ty.mk_descr;
+  Ty.diff Ty.any bool;
+  a;
+  Tags.construct(false, [TagComp.mk (Tag.mk "tag1", bool);TagComp.mk (Tag.mk "tag2", int)]) |> Descr.mk_tags |> Ty.mk_descr;
+  ]
+
+let list3 = [
+  TupleComp.mk [bool;int] |> Descr.mk_tuplecomp |> Ty.mk_descr;
+  Tuples.any |> Descr.mk_tuples |> Ty.mk_descr;
+  Tuples.construct (true, [TupleComp.mk [true_t;true_t]; TupleComp.mk [false_t;true_t]]) |> Descr.mk_tuples |> Ty.mk_descr;
+  TupleComp.mk [bool; true_t] |> Descr.mk_tuplecomp |> Ty.mk_descr;
+]
+
+let tx = Var.mk "'X"
+let vx = Ty.mk_var tx
+let x = Ty.of_eqs [(tx,Ty.cup ((vx, Ty.any) |> Descr.mk_arrow |> Ty.mk_descr) bool)] |> VarMap.of_list |> VarMap.find tx
+let x1 = Ty.cup ((x,Ty.any)|> Descr.mk_arrow |> Ty.mk_descr) true_t
+let list4 = [
+  x;
+  x1
+]
+let all_types = type_ints @ type_enums @ type_tags @ type_tuple @ type_arrows @ list1 @ list2 @list3 @list4
+
+
+
+let%expect_test _ = 
+  List.iter 
+  
+  (fun t -> let w = Witness.mk t in if Witness.is_in w t then 
+  Format.printf "@[%a : %a@]@\n" 
+  Printer.print_ty' t 
+  Witness.pp w 
+  else 
+    Format.printf "FALSE : %a is not a witness of %a\n" Witness.pp w Printer.print_ty' t) 
+  
+  all_types;
+  [%expect {|
+    int : 42
+    (1..) : 1
+    (..2) : 2
+    (7..10) : 7
+    (1..6) | (9..12) : 1
+    enum : " a "
+    False | True : " False "
+    enum \ (False | True) : " aaaaaa "
+    enum \ (aaa | aa | a) : " aaaa "
+    tag : a(16)
+    foo(int) : foo(42)
+    foo(int) : foo(42)
+    tag \ foo(int) : a(16)
+    tag \ (foo2(True | False) | foo(int)) : a(16)
+    foo2(True | False) | foo(int) : foo2(True)
+    foo(int) : foo(42)
+    tuple : ( 0 )
+    int, int : ( 42, 42 )
+    int, True | False, True | False : ( 42, True, True )
+    (int) | (int, int) | (True | False, True | False, True | False) : ( 42 )
+    tuple \ ((int) | (int, int) | (True | False, True | False, True | False)) : ( 0, 1, 2, 3 )
+    int, int : ( 42, 42 )
+    tuple \ (int, int) : ( 0 )
+    tuple \ (int, True | False, True | False) : ( 0 )
+    arrow : fun :< empty -> empty >
+    int -> int : fun :< int -> int >
+    tuple -> (int, int) : fun :< tuple -> int, int >
+    tuple -> (int, int) : fun :< tuple -> int, int >
+    arrow & ~(tuple \ (int, int) -> tuple \ (int, True | False, True | False)) & ~((int, True | False, True | False) -> (int) | (int, int) | (True | False, True | False, True | False)) : fun :< empty -> empty >
+    any : 42
+    True | False : " True "
+    True : " True "
+    False : " False "
+    tag(True | False) : tag(True)
+    ~(True | False) : 42
+    x1 where x1 = 42 | tag(x1) : 42
+    tag \ (tag2(int) | tag1(True | False)) : a(16)
+    True | False, int : ( True, 42 )
+    tuple : ( 0 )
+    True, True : ( True, True )
+    True | False, True : ( True, True )
+    x1 where x1 = True | False | (x1 -> any) : " True "
+    True | (x1 -> any) where x1 = True | False | (x1 -> any) : " True "
+    |}]
