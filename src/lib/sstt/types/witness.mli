2a7ea23f12b69117b4dac76b4ae99be3023d13b4
FredBelanger09 <fred.belanger09@laposte.net>
2026-04-13 17:40:42 +0200

    Ajout de la mémoïsation et nettoyage du code

--- src/lib/sstt/types/witness.mli
+++ src/lib/sstt/types/witness.mli
@@ -1,17 +1,17 @@
 open Core
 
-type single_t = Int of Z.t
+type t = private
+Int of Z.t
 |Enum of Enums.Atom.t
 |Arrow of Arrows.t 
-|Tuple of single_t list
-|Tag of (Tag.t * single_t)
+|Tuple of t list
+|Tag of (Tag.t * t)
 |Other
 |Wrong
-|Empty
 
 
 (**[singl_to_Ty t] transform the singleton type t in a real type Ty*)
-val singl_to_Ty : single_t -> Ty.t
+val to_ty : t -> Ty.t
 
 (**[mk_intervals t] return one value present in the interval part of t.
 Assume that the interval part of t is non-empty.
@@ -19,7 +19,7 @@ If t = ]-inf; +inf\[, return 42.
 If t as a part ]-inf; x], return x.
 Otherwise, return the lowest bound of t.
 *)
-val mk_intervals : Ty.t -> single_t
+val mk_intervals : Ty.t -> t
 
 (**[mk_enum t] return one value present  in the enum part of [t].
 Assume that the enum part of [t] is non-empty. 
@@ -27,7 +27,7 @@ If what is inside [t] is known, return the first element of [t].
 If what is NOT inside [t] is known, return the word composed of the letter 'a' repeated
 one more time than the max length of the atoms not in [t].
 *)
-val mk_enums : Ty.t -> single_t
+val mk_enums : Ty.t -> t
 
 (**[mk_tuple t] return one value present in the tuple part of [t].
 Assume that the tuple part of [t] is non-empty.
@@ -35,18 +35,17 @@ If the elements of [t] are known, return one inhabitant of one element of [t].
 If the elements of [not t] are known, return (0, 1,...,n, n+1) with n the arity max of the elements not in [t]. 
 *)
 
-
 (** [make t] returns one value that is in the type t. 
 Assume that t!= empty.
 *)
-val mk : Ty.t -> single_t
+val mk : Ty.t -> t
 
 (**[pp fmt w] is the pretty printer for the witness type*)
-val pp : Format.formatter -> single_t -> unit
+val pp : Format.formatter -> t -> unit
 
 (**[equal t1 t2] return [true] if the 2 witness [t1] and [t2] are equals, [false] otherwise.*)
-val equal : single_t -> single_t -> bool
+val equal : t -> t -> bool
 
 (**[is_in w t] return [true] if the witness [w] is an inhabitant of [t], [false] otherwise.
     Create the type t' that only has w in it, then check if t' is a subtype of t.*)
-val is_in : single_t -> Ty.t -> bool
+val is_in : t -> Ty.t -> bool
