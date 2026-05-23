cfb15a53794704284333dacc59e9b1dc48fa1e3e
Mickael Laurent <esh4rk@gmail.com>
2026-05-23 11:30:22 +0200

    update Sigs and OTy module

--- src/lib/sstt/core/components/fields.ml
+++ src/lib/sstt/core/components/fields.ml
@@ -2,17 +2,35 @@ open Base
 open Sigs
 open Sstt_utils
 
-module OTy(N:Node) = struct
+module OAtom(N:Node) = struct
   type node = N.t
   type t = node * bool
 
+  let map_nodes f (n,b) = (f n, b)
+  let direct_nodes (n,_) = [n]
+  let simplify t = t
+  let equal (n1,b1) (n2,b2) =
+    Bool.equal b1 b2 && N.equal n1 n2
+  let compare (n1,b1) (n2,b2) =
+    Bool.compare b1 b2 |> ccmp
+      N.compare n1 n2
+  let equal' f (n1,b1) (n2,b2) = Bool.equal b1 b2 && f n1 n2
+  let compare' f (n1,b1) (n2,b2) = Bool.compare b1 b2 |> ccmp f n1 n2
+  let hash (n, b) = Hash.(mix (bool b) (N.hash n))
+  let hash' f (n, b) = Hash.(mix (bool b) (f n))
+
+  let is_optional (_,b) = b
+  let is_required (_,b) = not b
+
   let any = (N.any, true)
   let empty = (N.empty, false)
+  let present = (N.any, false)
   let absent = (N.empty, true)
   let required t = (t, false)
   let optional t = (t, true)
   let get (t,_) = t
 
+  (* Set-theoretic operations *)
   let cap (n1, b1) (n2, b2) = (N.cap n1 n2, b1 && b2)
   let cap = fcap ~empty ~any ~cap
   let cup (n1, b1) (n2, b2) = (N.cup n1 n2, b1 || b2)
@@ -27,27 +45,56 @@ module OTy(N:Node) = struct
   let disj lst =
     let ns, bs = List.split lst in
     (N.disj ns, List.fold_left (||) false bs)
-
   let is_empty (n,b) = not b && N.is_empty n
-  let is_any (n,b) = b && N.is_any n
-  let is_absent (n,b) = b && N.is_empty n
-  let is_optional (_,b) = b
-  let is_required (_,b) = not b
-  let leq (n1,b1) (n2,b2) = (not b1 || b2) && N.leq n1 n2
-  let equiv (n1,b1) (n2,b2) = b1 = b2 && N.equiv n1 n2
-  let disjoint (n1,b1) (n2,b2) = not (b1 && b2) && N.disjoint n1 n2
+end
 
-  let equal (n1,b1) (n2,b2) = b1 = b2 && N.equal n1 n2
-  let equal' f (n1,b1) (n2,b2) = b1 = b2 && f n1 n2
-  let compare (n1,b1) (n2,b2) = Bool.compare b1 b2 |> ccmp N.compare n1 n2
-  let compare' f (n1,b1) (n2,b2) = Bool.compare b1 b2 |> ccmp f n1 n2
+module OTy(N:Node) = struct
+  module Atom = OAtom(N)
+  module BoolLeaf = Bdd.BoolLeaf
+  module Bdd = Bdd.Make(Atom)(BoolLeaf)
 
-  let map_nodes f (n,b) = (f n, b)
-  let direct_nodes (n,_) = [n]
-  let simplify t = t
+  type t = Bdd.t
+  type node = N.t
 
-  let hash (n, b) = Hash.(mix (bool b) (N.hash n))
-  let hash' f (n, b) = Hash.(mix (bool b) (f n))
+  let any = Bdd.any
+  let empty = Bdd.empty
+  let mk a = Bdd.singleton a
+
+  let cap = Bdd.cap
+  let cup = Bdd.cup
+  let neg = Bdd.neg
+  let diff = Bdd.diff
+
+  let get (ps,ns) =
+    Atom.diff (Atom.conj ps) (Atom.disj ns)
+  let is_clause_empty (ps,ns,b) =
+    not b || Atom.is_empty (get (ps,ns))
+  let is_empty t = Bdd.for_all_lines is_clause_empty t
+  let get = Bdd.fold_lines (fun acc (ps,ns,b) ->
+    if b then Atom.cup acc (get (ps,ns)) else acc) Atom.empty
+
+  let equal = Bdd.equal
+  let compare = Bdd.compare
+  let hash = Bdd.hash
+  let equal' f = Bdd.equal' (Atom.equal' f) (BoolLeaf.equal)
+  let compare' f = Bdd.compare' (Atom.compare' f) (BoolLeaf.compare)
+  let hash' f = Bdd.hash' (Atom.hash' f) (BoolLeaf.hash)
+
+  let leq t1 t2 = diff t1 t2 |> is_empty
+  let equiv t1 t2 = leq t1 t2 && leq t2 t1
+  module Comp = struct
+    type atom = Atom.t
+    let atom_is_valid _ = true
+    let leq t1 t2 = leq (Bdd.of_dnf t1) (Bdd.of_dnf t2)
+  end
+  module Dnf = Dnf.LMake(Comp)
+  let dnf t = N.with_own_cache (fun t -> Bdd.dnf t |> Dnf.export) t
+  let of_dnf dnf = N.with_own_cache (fun dnf -> Dnf.import dnf |> Bdd.of_dnf) dnf
+
+  let direct_nodes t = Bdd.atoms t |> List.concat_map Atom.direct_nodes
+  let map_nodes f t = Bdd.map_nodes (Atom.map_nodes f) t
+  let map f t = Bdd.map_nodes f t
+  let simplify t = Bdd.simplify equiv t
 end
 
 module Make(N:Node) = struct
