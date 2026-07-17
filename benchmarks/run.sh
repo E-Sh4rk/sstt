8b048717a60113e58b6633f4e0ea341db9ea5464
Kim Nguyễn <kim.nguyen@universite-paris-saclay.fr>
2026-07-17 09:46:11 +0200

    Generate the prelude.

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -5,7 +5,43 @@ then
     echo "Script must be run from the project root"
     exit 1
 fi
+
+trap ctrl_c INT
+
+ctrl_c () {
+    test -f "$CFG".backup && mv "$CFG".backup "$CFG"
+}
+
 CFG=src/lib/sstt/core/utils/config.ml
+CORPUS_A=benchmarks/0_hm.json
+CORPUS_B=benchmarks/1_union_inter.json
+CORPUS_C=benchmarks/2_dyn.json
+echo " \\GRAYLINE    & Building  
+ \\GRAYLINE    & Solving   
+ \\GRAYLINE A.   & Total     
+ \\GRAYLINE    & \\# Sol.
+ \\GRAYLINE  $(cat ${CORPUS_A} | grep '\"vars\"' | wc -l) & Size      
+ \\GRAYLINE instances   & Avg. Size 
+ \\GRAYLINE    & Peak Size 
+ \\GRAYLINE    & Timeout   
+              & Building  
+              & Solving   
+          B.  & Total
+	      & \\# Sol.
+         $(cat ${CORPUS_B} | grep '\"vars\"' | wc -l)     & Size      
+         instances & Avg. Size 
+              & Peak Size 
+              & Timeout   
+ \\GRAYLINE    & Building  
+ \\GRAYLINE   & Solving   
+ \\GRAYLINE C. & Total
+ \\GRAYLINE   & \# Sol.
+ \\GRAYLINE $(cat ${CORPUS_C} | grep '\"vars\"' | wc -l) & Size      
+ \\GRAYLINE instances  & Avg. Size 
+ \\GRAYLINE    & Peak Size 
+ \\GRAYLINE    & Timeout   " > benchmarks/00_prelude.log
+
+
 
 for c in benchmarks/config/*.pre
 do
@@ -14,13 +50,13 @@ do
     rm -f "$output"
     cp "$CFG" "$CFG".backup
     cp "$c" "$CFG"
-    for b in benchmarks/[0-2]*.json
+    for b in ${CORPUS_A} ${CORPUS_B} ${CORPUS_C}
     do
 	echo "   Input $(basename $b)"
 	sed -i "$CFG" -e 's/let *benchmark_size *= .*/let benchmark_size = false/'
-	dune exec --display=quiet -- src/bin/benchmark.exe "$b" | grep -v 'space\|errors' | cut -f 2 -d ':' >> "$output"
+	opam exec -- dune exec --display=quiet -- src/bin/benchmark.exe "$b" | grep -v 'space\|errors' | cut -f 2 -d ':' >> "$output"
 	sed -i "$CFG" -e 's/let *benchmark_size *= .*/let benchmark_size = true/'
-	dune exec --display=quiet -- src/bin/benchmark.exe "$b" | grep 'space\|errors' | cut -f 2 -d ':' >> "$output"
+	opam exec -- dune exec --display=quiet -- src/bin/benchmark.exe "$b" | grep 'space\|errors' | cut -f 2 -d ':' >> "$output"
     done
     mv "$CFG".backup "$CFG"
 done
